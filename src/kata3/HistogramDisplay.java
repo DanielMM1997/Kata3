4b2819e4990c1d5865e8c3e3551731c237d24759
Usuario <Usuario@10.22.148.84>
2019-10-23 12:18:18 +0100

    kata 3 version 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -11,32 +11,51 @@ import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
 
-    public HistogramDisplay() {
-        super("HISTOGRAMA");
+    private final Histogram<String> histogram;
+    
+    public HistogramDisplay(Histogram histogram) {
+        super("HSITOGRAMA");
+        this.histogram = histogram;
         this.setContentPane(createPanel());
-        this.pack();        
+        this.pack();
     }
     
     public void execute() {
         setVisible(true);
     }
-
+    
     private JPanel createPanel() {
-        ChartPanel chartpanel = new ChartPanel(createChart(createDataSet()));
-        chartpanel.setPreferredSize(new Dimension(500, 400));
-        return chartpanel;
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension (500,400));
+        return chartPanel;
     }
-
-    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
-        JFreeChart chart = ChartFactory.createBarChart("histograma JFreeChart", "Dominios email", "N de emails", dataSet, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
+    
+    private JFreeChart createChart (DefaultCategoryDataset dataSet) {
+        JFreeChart chart;
+        chart = ChartFactory.createBarChart("Histograma JFreeChart", 
+                "Dominio email",
+                "Nยบ de emails",
+                dataSet,
+                PlotOrientation.VERTICAL,
+                false,
+                false,
+                rootPaneCheckingEnabled);
+        
         return chart;
     }
     
-    private DefaultCategoryDataset createDataSet() {
+    private DefaultCategoryDataset createDataset() {
+        
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }                
+        /*
         dataSet.addValue(300, "", "ulpgc.es");
-        dataSet.addValue(200, "", "ulpgc.ulpgc.es");
-        dataSet.addValue(800, "", "ulpgc.dis.es");
+        dataSet.addValue(400, "", "dis.ulpgc.es");
+        dataSet.addValue(200, "", "gmail.com");
+        */
         return dataSet;
     }
 }
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,7 +3,18 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histogramdisplay = new HistogramDisplay();
+        Histogram histogram = new Histogram();
+        
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("gmail.com");
+        
+        HistogramDisplay histogramdisplay = new HistogramDisplay(histogram);
         histogramdisplay.execute();
     }
     
